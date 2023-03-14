3f0b237991ed8333ef802dec9c64252a074ac49a
innocent259 <ndahiminnocent@gmail.com>
2023-03-14 09:37:14 +0200

    Modify index.js

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const div = document.createElement('div');
 div.className = 'new-container';
 div.innerHTML = `
-<a href="#" class="button-img"> 
-    <img src = "./images/Cancel.svg" alt="Cancel-icon" class="img-cancel" />
-</a>
-<ul class="ul-items">
-    <li>
-        <a href="#">Portfolio</a>
-        <hr>
-    </li>
-    <li>
-        <a href="#">About</a>
-        <hr>
-    </li>
-    <li>
-        <a href="#">Contact</a>
-        <hr>
-    </li>   
-</ul>
-<div class="ending">
-    <hr>
+<div class="new-element-container" >
+    <span class="button-and-list">
+        <a href="#" class="button-img"> 
+            <img src = "./images/Cancel.svg" alt="Cancel-icon" class="img-cancel" />
+        </a>
+        <ul class="ul-items">
+            <li>
+                <a href="#">Portfolio</a>
+                <hr>
+            </li>
+            <li>
+                <a href="#">About</a>
+                <hr>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+                <hr>
+            </li>   
+        </ul>
+    </span>
+    <span class="ending">
+        <hr class="new-line-2">
+    </span>
 </div>
 `;
 const menu = document.querySelector('.nav-menu');
@@ -31,7 +35,6 @@ const removeItems = div.querySelector('.button-img');
 function removeChild() {
   div.parentNode.removeChild(div);
 }
-
 menu.addEventListener('click', handleClick);
 
 removeItems.addEventListener('click', removeChild);
