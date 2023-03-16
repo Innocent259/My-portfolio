7b64a371a2e241b4051e99edf15dd72cbfe5f339
MesakDuduCoder <duduaoa99@gmail.com>
2023-03-16 22:15:44 +0530

    Add form submit function

--- form.js
+++ form.js
@@ -3,4 +3,11 @@ const emailValid = document.querySelector('#email-feedback-yes')
 const emailInvalid = document.querySelector('#email-feeback-wrong')
 const form = document.querySelector('.form-form') 
 
-
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (emailCheck === emailCheck.toLowerCase()) {
+    form.submit();
+  } else {
+    emailInvalid.textContent = "Form will not be submitted check email again";
+  }
+});
