ced212e935e0b4467c0e5c18e440fed43d1acac1
innocent259 <ndahiminnocent@gmail.com>
2023-03-16 18:41:04 +0200

    Add email validation

---
+++ form.js
@@ -0,0 +1,6 @@
+const emailCheck = document.querySelector('#email-valid').value;
+const emailValid = document.querySelector('#email-feedback-yes')
+const emailInvalid = document.querySelector('#email-feeback-wrong')
+const form = document.querySelector('.form-form') 
+
+
