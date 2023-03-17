99b090736468f75c52bebfbcad197f99a6aea7d7
innocent259 <ndahiminnocent@gmail.com>
2023-03-17 15:52:11 +0200

    Add local storage to user inputs

---
+++ localStorage.js
@@ -0,0 +1,22 @@
+const nameInput = document.querySelector('#name-id');
+const emailInput = document.querySelector('#email-valid');
+const messageInput = document.querySelector('#text-id');
+
+function formData() {
+  const formData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+[nameInput, emailInput, messageInput].forEach((input) => {
+  input.addEventListener('input', formData);
+});
+const savedFormData = JSON.parse(localStorage.getItem('formData'));
+if (savedFormData) {
+  nameInput.value = savedFormData.name;
+  emailInput.value = savedFormData.email;
+  messageInput.value = savedFormData.message;
+}
